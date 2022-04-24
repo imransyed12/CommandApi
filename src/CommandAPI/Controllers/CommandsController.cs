d492ef0064df517f50ad8e16a53115a661bee4ac
imransyed12 <imransdmca@gmail.com>
2022-04-24 22:44:46 +0530

    [Added Database Context and Code First approach]

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -23,5 +23,16 @@ namespace CommandAPI.Controllers
 
             return Ok(commandItems);
         }
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommand(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            if (commandItem == null)
+            {
+            return NotFound();
+            }
+            return Ok(commandItem);
+        }
     }
 }
