9511c1832c4f79ba37b38df48704a23cbd230a3a
imransyed12 <imransdmca@gmail.com>
2022-05-01 10:25:22 +0530

    [Removed the user and password]

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace CommandAPI
 {
@@ -27,8 +28,13 @@ namespace CommandAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+            
             services.AddDbContext<CommandContext>(opt => 
-                        opt.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection")));
+                        opt.UseNpgsql(builder.ConnectionString));
             services.AddControllers();
             services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
             services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
